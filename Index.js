46fc611b66ea5f3297aa18ff880e52dc16e34cba
Ditsuhe <aquinomarkrico29@gmail.com>
2024-05-25 12:40:10 +0800

    Update Index.js

--- Index.js
+++ Index.js
@@ -3,7 +3,7 @@ const btnLike2 = document.getElementById("btnLike2")
 const countLikes1 = document.getElementById("countLikes1")
 const countLikes2 = document.getElementById("countLikes2")
 
-function hitlikes1() {
+function hitLikes1() {
    let totalLikes1 = parseInt(countLikes1.value) +1
    countLikes1.textContent = totalLikes1.toString()  
 }
