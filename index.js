d2bc649196c2b1f2cf29c90b496f81ed88053cf6
Ditsuhe <aquinomarkrico29@gmail.com>
2024-06-08 11:52:16 +0800

    Update index.js

--- index.js
+++ index.js
@@ -37,5 +37,6 @@ const comment = document.getElementById("comment")
 const commentbox = document.getElementById("commentbox")
 function submitComment(){
   commentbox.textContent +=comment.value.toString() + "\n"
+  comment.value=""; 
 }
 submit.addEventListener("click",submitComment)
