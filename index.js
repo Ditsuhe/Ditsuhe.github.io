93e7d8eebeb2487fb8666fe858ed93e3d1c851a3
Ditsuhe <aquinomarkrico29@gmail.com>
2024-06-08 11:19:56 +0800

    Update index.js

--- index.js
+++ index.js
@@ -31,3 +31,11 @@ function hitdisLikes2() {
   countdisLikes2.textContent = totaldisLikes2.toString()
 }
 btndisLike2.addEventListener("click",hitdisLikes2)
+
+const submit = document.getElementById("submit")
+const comment = document.getElementById("comment")
+const commentbox = document.getElementById("commentbox")
+function submitComment(){
+  commentbox.textContent +=comment.value.toString() + "\n"
+}
+submit.addEventListener("click",submitComment)
